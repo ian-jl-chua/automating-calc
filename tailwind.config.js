a91f07691997b52972fa0556aa114b362e884b46
Ian Chua <ianchuajl@hotmail.com>
2025-01-19 20:58:13 +1300

    baisc set up done

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './assets/js/*.js', './assets/css/*.css'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
